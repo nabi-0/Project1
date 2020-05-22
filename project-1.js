16585df6374a03cce3335ece6362aa7fa5791c0f
Journey Cruz <journey.cruz@gmail.com>
2020-05-21 22:23:53 -0500

    JS file has been reformatted to fit new API , Added jQuery link to index.html

--- project-1.js
+++ project-1.js
@@ -1,14 +1,19 @@
 function getAthleteInfo() {
 
-    var athlete = $(this).attr("data-name");
-    var queryURL = "insert api" + athlete + "insert api key";
-
-    $.ajax({
-      url: queryURL,
-      method: "GET"
-    }).then(function(response) {
-        console.log(response);
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://free-nba.p.rapidapi.com/players/3092",
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "free-nba.p.rapidapi.com",
+      "x-rapidapi-key": "75707fa799msha4a06e78acf5fdcp1f1ec2jsn683a106045e2"
     }
+  }
+  
+  $.ajax(settings).then(function (response) {
+    console.log(response);
+  });
 }
 
 getAthleteInfo();

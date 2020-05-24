4d558a660d6d07289ef37d59d973fb2a802dcad6
Journey Cruz <journey.cruz@gmail.com>
2020-05-24 14:37:27 -0500

    Add API and function for soccer players

--- project-1.js
+++ project-1.js
@@ -1,4 +1,4 @@
-function getAthleteInfo() {
+function getBasketballInfo() {
 
   var settings = {
     "async": true,
@@ -16,4 +16,24 @@ function getAthleteInfo() {
   });
 }
 
-getAthleteInfo();
+function getSoccerInfo() {
+
+  var settings = {
+    "async": true,
+    "crossDomain": true,
+    "url": "https://api-football-v1.p.rapidapi.com/v2/players/player/567",
+    "method": "GET",
+    "headers": {
+      "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
+      "x-rapidapi-key": "75707fa799msha4a06e78acf5fdcp1f1ec2jsn683a106045e2"
+    }
+  }
+  
+  $.ajax(settings).done(function (response) {
+    console.log(response);
+  });
+
+}
+
+getBasketballInfo();
+getSoccerInfo();

c872f120e362c1b7c4865c5a1e34f95073ca49c9
Journey Cruz <journey.cruz@gmail.com>
2020-05-28 20:52:07 -0500

    Added Correct API's and functions that make ajax calls and console log information.

--- project-1.js
+++ project-1.js
@@ -1,39 +1,87 @@
-function getBasketballInfo() {
-
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://free-nba.p.rapidapi.com/players/3092",
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "free-nba.p.rapidapi.com",
-      "x-rapidapi-key": "75707fa799msha4a06e78acf5fdcp1f1ec2jsn683a106045e2"
-    }
-  }
-  
-  $.ajax(settings).then(function (response) {
-    console.log(response);
-  });
+//var q = "";
+// for testing
+var q = "James";
+//var isActive = "";
+//for testing
+var isActive = "Y"
+ 
+var sport = "";
+ 
+ 
+// on click of dropdown (or options?) get selected option
+$("#sportDropdown li").on("click", function() {
+   var sport = $(this).text();
+   console.log(sport);
+ 
+   if (sport == "MLB Baseball - Inactive") {
+       isActive = "N";
+ 
+   }
+   if (sport == "MLB Baseball - Active") {
+       isActive = "Y";
+   }
+ 
+   // close on click?
+   $('.dropdown-content').foundation('close');
+})
+ 
+// on click of isActive dropdown, get selection option (Y/N)
+$("isActiveBtn").on("click", function() {
+   var isActive = this.text;
+   console.log(isActive);
+})
+ 
+// on click of search button, get search and based on sport selection, run api
+$("#searchBtn").on("click", function() {
+   console.log("test");
+   q = document.getElementById("searchBar").value;
+   //console.log(q);
+   //isActive = document.getElementById("isActive").value;
+ 
+   // check sport selection
+   if (sport == "NBA Basketball") {
+       console.log("basketball");
+       getBasketballInfo(q);
+   }
+   if (sport == "MLB Baseball") {
+       console.log("baseball");
+       getBaseballInfo(q, isActive);
+   }
+});
+ 
+// basketball player search
+function getBasketballInfo(q) {
+   var settings = {
+       "async": true,
+       "crossDomain": true,
+       "url": "https://free-nba.p.rapidapi.com/players?page=0&per_page=25&search=" + q,
+       "method": "GET",
+       "headers": {
+           "x-rapidapi-host": "free-nba.p.rapidapi.com",
+           "x-rapidapi-key": "75707fa799msha4a06e78acf5fdcp1f1ec2jsn683a106045e2"
+       }
+   }
+ 
+   $.ajax(settings).then(function(response) {
+       console.log(response);
+   });
 }
-
-function getSoccerInfo() {
-
-  var settings = {
-    "async": true,
-    "crossDomain": true,
-    "url": "https://api-football-v1.p.rapidapi.com/v2/players/player/567",
-    "method": "GET",
-    "headers": {
-      "x-rapidapi-host": "api-football-v1.p.rapidapi.com",
-      "x-rapidapi-key": "75707fa799msha4a06e78acf5fdcp1f1ec2jsn683a106045e2"
-    }
-  }
-  
-  $.ajax(settings).done(function (response) {
-    console.log(response);
-  });
-
-}
-
-getBasketballInfo();
-getSoccerInfo();
+ 
+//baseball player search
+function getBaseballInfo(q, isActive) {
+   var settings = {
+       "async": true,
+       "crossDomain": true,
+       "url": "http://lookup-service-prod.mlb.com/json/named.search_player_all.bam?sport_code='mlb'&active_sw=" + "'" + isActive + "'" + "&name_part='" + q + "%25'",
+   }
+   $.ajax(settings).done(function(response) {
+       console.log(response);
+    
+   });
+};
+ 
+//function getSeason() {};
+ 
+//getBasketballInfo(q);
+getBaseballInfo(q, isActive);
+ 
